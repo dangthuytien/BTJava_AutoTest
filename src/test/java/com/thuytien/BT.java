5df6e2b7c7572e414eadf2f106f50eed25bea10c
DangThiThuyTien <dangthuytien1711@gmail.com>
2024-10-03 22:49:29 +0700

    push
    Add changes

---
+++ src/test/java/com/thuytien/BT.java
@@ -0,0 +1,4 @@
+package com.thuytien;
+
+public class BT {
+}
---
+++ src/test/java/com/thuytien/BTLocator/LocatorCRM.java
@@ -0,0 +1,28 @@
+package com.thuytien.BTLocator;
+
+public class LocatorCRM {
+
+    //Xpath trang Login
+
+    public static String headerLogo = "//img[@alt='Perfex CRM | Anh Tester Demo']";
+    public static String headerLogin = "//h1[normalize-space()='Login']";
+    public static String inputEmail = "//input[@id='email']";
+    public static String inputPassword = "//input[@id='password']";
+    public static String checkboxRememberMe = "//input[@id='remember']";
+    public static String buttonLogin = "//button[@type='submit']";
+    public static String linkTextForgotPassword = "//a[normalize-space()='Forgot Password?']";
+
+    //Xpath menu Customer
+
+    public static String menuCustomers = "//span[normalize-space()='Customers']";
+    //
+    public static String buttonHideMenu = "//i[@class='fa fa-align-left']";
+    public static String inputTopSearch = "//input[@id='search_input']";
+    public static String buttonTopSearch = "//button[starts-with(@class, 'tw-outline-none')]";
+    public static String buttonQuickCreate = "//span[starts-with(@class, 'tw-rounded-full')]";
+
+    //Xpath trang New Customer
+    
+
+
+}
